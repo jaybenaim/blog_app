a6637c2b3b672c8be387a133c3ad18895f8dfad1
Jay Benaim <benaimjacob@gmail.com>
2019-06-18 21:06:16 -0400

    added nav to header.ejs

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.get("/", function(req,res) {
     res.redirect("/blogs"); 
 }); 
 
+// INDEX ROUTE 
 app.get("/blogs", function(req,res) {
      Blog.find({}, function(err, blogs){ 
         if(err) { 
@@ -39,7 +40,33 @@ app.get("/blogs", function(req,res) {
         }
     })
 }); 
+// NEW ROUTE 
+app.get("/blogs/new", function(req,res) { 
+    res.render("new"); 
+}); 
 
+// CREATE ROUTE 
+app.post("/blogs", function(req,res) { 
+    Blog.create(req.body.blog, function(err, newBlog) { 
+        if(err) { 
+            res.render("new"); 
+        } else {
+            res.redirect("/blogs"); 
+        }
+    }); 
+}); 
+
+// SHOW ROUTE 
+app.get("/blogs/:id", function(req, res){ 
+    Blog.findById(req.params.id, function(err, foundBlog){ 
+        if(err){ 
+            res.redirect("/blogs"); 
+        } else { 
+            res.render("show", {blog: foundBlog}); 
+        }
+    })
+
+}); 
 app.listen(3000, function() { 
     console.log("Servers up!"); 
 })
