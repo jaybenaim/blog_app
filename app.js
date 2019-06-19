4d80ae682b046ded913f49b5d7a5744d7a345af5
Jay Benaim <benaimjacob@gmail.com>
2019-06-19 12:36:03 -0400

    added express sanitze to create and update routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const   express         = require('express'), 
-        methodOverride  = require("method-override"), 
+        methodOverride  = require("method-override"),
+        expressSanitizer = require('express-sanitizer'),  
         mongoose        = require('mongoose'), 
         bodyParser      = require('body-parser'), 
         app             = express(); 
@@ -11,7 +12,9 @@ mongoose.connect("mongodb://localhost/blog_app", { useNewUrlParser: true , useFi
 app.set("view engine", "ejs"); 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer()); 
 app.use(methodOverride("_method")); 
+
 
 
 
@@ -53,6 +56,7 @@ app.get("/blogs/new", function(req,res) {
 
 // CREATE ROUTE 
 app.post("/blogs", function(req,res) { 
+    req.body.blog.body = req.sanitize(req.body.blog.body); 
     Blog.create(req.body.blog, function(err, newBlog) { 
         if(err) { 
             res.render("new"); 
@@ -86,6 +90,7 @@ app.get("/blogs/:id/edit", function(req,res) {
 
 // UPDATE ROUTE 
 app.put("/blogs/:id", function(req, res) { 
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) { 
         if(err){ 
             res.redirect("/blogs"); 
